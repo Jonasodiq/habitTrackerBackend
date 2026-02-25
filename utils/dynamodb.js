4d9f72fe2678965daf6f325e4f80f6e711d13b65
Jonasodiq <jonasniyazson@hotmail.com>
2026-02-25 11:22:00 +0100

    Added DDB client

---
+++ utils/dynamodb.js
@@ -0,0 +1,7 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({ region: 'eu-north-1' });
+const dynamo = DynamoDBDocumentClient.from(client);
+
+module.exports = dynamo;
